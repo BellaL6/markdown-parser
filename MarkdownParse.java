ca062930896ede8c4ccb99462c5329fa9e251f7f
BellaL6 <jyli@ucsd.edu>
2022-04-21 16:03:32 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,13 +16,6 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            
-
-
-            if (markdown.charAt(openBracket - 1) == ('!')) {
-                currentIndex = closeParen + 1;
-                continue;
-            }
 
 
             toReturn.add(markdown.substring(openParen + 1, closeParen));
