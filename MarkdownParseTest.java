8426abc39d9edff5bfd885df18077f05bd84bb19
BellaL6 <jyli@ucsd.edu>
2022-04-21 19:31:53 -0700

    tester

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,29 @@
 import static org.junit.Assert.*;
+
+import javax.swing.text.AbstractDocument.Content;
+
 import org.junit.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
         
+    }
+
+    @Test
+    public void testSepUp() throws IOException {
+        
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+    
+        assertEquals(List.of("https://something.com", "some-thing.html"), links);
     }
 }
